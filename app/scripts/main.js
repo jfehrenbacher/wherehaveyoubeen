bfa6a29e374ac850376e2b7bd8b6229113185eb6
jfehrenbacher <fehrenb1@seattleu.edu>
2016-12-13 14:32:39 -0800

    Got data-target-toggle working, changed ids, about to add images

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,30 @@
 console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
 
 
-$('.minibox').on('click', function (){
-	var txt = $(this).attr('name');
-	$('ul').append("<li class='list'>"+txt+"</li>");
-	$(this).attr
+// $('.minibox').on('click', function (){
+// 	var txt = $(this).attr('name');
+// 	$('ul').append("<li class='list'>"+txt+"</li>");
+// 	$(this).toggleClass('selected');
+// 	// $('li').toggleClass(this).attr('id');
+
+// });
+
+
+// $('#dialog').dialog({ autoOpen: false })
+// $('.list').click(function() {
+// 	$('#dialog').dialog({
+// 		modal:true,
+// 	});
+// 	$('#dialog').dialog('open');
+// });
+
+$('.minibox').click(function() {
 	$(this).toggleClass('selected');
+	var targetClass = $(this).data("target-toggle");
+	var targetElement = $("." + targetClass);
+	if (targetElement.hasClass('noshow') ) { 
+	targetElement.addClass('show').removeClass('noshow');
+	} else {
+		targetElement.removeClass('show').addClass('noshow');
+	} 
 });
-
-$('ul').on('click','.list', function(){
-	$(this).remove();
-});
-
