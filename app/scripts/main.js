df513548618a92dcfe48768b4645754b7a2cf25c
jfehrenbacher <fehrenb1@seattleu.edu>
2016-12-13 10:04:21 -0800

    New branch, tons of work done, commit more often dummy

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,14 @@
 console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+
+
+$('.minibox').on('click', function (){
+	var txt = $(this).attr('name');
+	$('ul').append("<li class='list'>"+txt+"</li>");
+	$(this).attr
+	$(this).toggleClass('selected');
+});
+
+$('ul').on('click','.list', function(){
+	$(this).remove();
+});
+
