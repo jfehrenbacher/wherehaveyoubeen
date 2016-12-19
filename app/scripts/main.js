fb13bae0d8f7fb33b27695c923e091e5fdc36bb0
jfehrenbacher <fehrenb1@seattleu.edu>
2016-12-19 15:20:18 -0800

    Working on submit.html, localStorage

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -27,24 +27,109 @@ $('.minibox').click(function() {
 	} else {
 		targetElement.removeClass('show').addClass('noshow');
 	} 
+	var cap = $('.selected.cap').length;
+	console.log('Capitol Hill Venues:' + cap);
+	var fre = $('.selected.fre').length;
+	console.log('Fremont Venues:' + fre);
+	var bal = $('.selected.bal').length;
+	console.log('Ballard Venues:' + bal);
+	var udist = $('.selected.udist').length;
+	console.log('U District Venues:' + udist);
+	var downtown = $('.selected.downtown').length;
+	console.log('Downtown Venues:' + downtown);
+	var columbia = $('.selected.columbia').length;
+	console.log('Columbia Venues:' + columbia);
+	var sodo = $('.selected.sodo').length;
+	console.log('SODO Venues:' + sodo);
+	var neighborhoodarray = [cap, fre, bal, udist, downtown, columbia, sodo];
+	console.log(neighborhoodarray);
+	var largest = Math.max.apply(Math, neighborhoodarray);
+	console.log(largest);
+	var smallest = Math.min.apply(Math, neighborhoodarray);
+	console.log(smallest);
+	if (largest == cap) {
+		localStorage.setItem('largest', 'caphill');
+	} else if (largest == fre) {
+		localStorage.setItem('largest', 'fremont');
+	} else if (largest == bal) {
+		localStorage.setItem('largest', 'ballard');
+	} else if (largest == udist) {
+		localStorage.setItem('largest', 'udistrict');
+	} else if (largest == downtown) {
+		localStorage.setItem('largest', 'downtown');
+	} else if (largest == columbia) {
+		localStorage.setItem('largest', 'columbia');
+	} else if (largest == sodo) {
+		localStorage.setItem('largest', 'sodo');
+	} 
 });
 
+// $('.submitbutton').click(function() {
+// 	var bigstorage = localStorage.getItem('largest');
+// 	if (bigstorage = 'caphill') {
+// 		$('#caphillpage').removeClass('noshow').addClass('show');
+// 	}
+// })
+
+$(document).ready(function() {
+	var bigstorage = localStorage.getItem('largest');
+	if (bigstorage =='caphill') {
+		$('#caphillpage').removeClass('noshow').addClass('show');
+		$('#caphilllist').removeClass('show').addClass('noshow');
+	} else if (bigstorage == 'fremont') {
+		$('#frepage').removeClass('noshow').addClass('show');
+		$('#frelist').removeClass('show').addClass('noshow');
+	} else if (bigstorage == 'ballard') {
+		$('#balpage').removeClass('noshow').addClass('show');
+		$('#ballist').removeClass('show').addClass('noshow');
+	} else if (bigstorage == 'udistrict') {
+		$('#udistpage').removeClass('noshow').addClass('show');
+		$('#udistlist').removeClass('show').addClass('noshow');
+	} else if (bigstorage == 'downtown') {
+		$('#dtpage').removeClass('noshow').addClass('show');
+		$('#dtlist').removeClass('show').addClass('noshow');
+	} else if (bigstorage == 'columbia') {
+		$('#ccpage').removeClass('noshow').addClass('show');
+		$('#cclist').removeClass('show').addClass('noshow');
+	} else if (bigstorage == 'sodo') {
+		$('#sodopage').removeClass('noshow').addClass('show');
+		$('#sodolist').removeClass('show').addClass('noshow');
+	} 
+})
 
 
-$('#clickMe').click(function(event) {
-    var mytext = $('#myText').val();
+// $('.minibox').click(function classCount() {
+// 	var cap = ($('.selected.cap').length);
+// 	console.log('Capitol Hill Venues:' + cap);
+// 	var name = $(this).attr('id');
+// 	// var id = 1;
+// 	// var test = localStorage.getItem("test");
+// 	// var obj = [];
+// 	// if(test){
+// 	// 	obj= JSON.parse(test);
+// 	// }
+// 	// obj.push({"id": id, "name":name});
+// 	// localStorage.setItem("test", JSON.stringify(obj));
+// 	console.log(name);
+// });
 
 
-    $('<div id="dialog">'+mytext+'</div>').appendTo('#page');        
-    event.preventDefault();
 
-        $("#dialog").dialog({                   
-            width: 600,
-            modal: true,
-            close: function(event, ui) {
-                $("#dialog").remove();
-                }
-            });
-    }); //close click
+
+// $('#clickMe').click(function(event) {
+//     var mytext = $('#myText').val();
+
+
+//     $('<div id="dialog">'+mytext+'</div>').appendTo('#page');        
+//     event.preventDefault();
+
+//         $("#dialog").dialog({                   
+//             width: 600,
+//             modal: true,
+//             close: function(event, ui) {
+//                 $("#dialog").remove();
+//                 }
+//             });
+//     }); //close click
 
 
